493894baab54252a38c0baa4ae7bfe59ab2c9a12
mainsailstudio <design@mainsailstudio.com>
2018-02-21 10:38:18 -0500

    Testing API on remote azure server

--- dynauthapi/api.go
+++ dynauthapi/api.go
@@ -8,128 +8,120 @@ package api
 
 import (
 	"encoding/json"
-	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
-// Auth structure - each individual authentication word
-type Auth struct {
-	ID   string `json:"id"`
-	Auth string `json:"auth"`
-	Salt string `json:"salt"`
-}
-
-// Key structure - each key that attaches directly to an auth via authID
-type Key struct {
-	ID     string `json:"id"`
-	AuthID string `json:"authid"`
-}
-
-// Delim stucture - the delimiter between each key + auth pair
-type Delim struct {
-	ID    string `json:"id"`
-	Delim string `json:"delim"`
-}
-
-// Hash structure - the entire hash with keys, delimiters, and auths
-type Hash struct {
-	HashID string `json:"hashid"`
-	Auth   *Auth  `json:"auth"`
-	Key    *Key   `json:"key"`
-	Delim  *Delim `json:"delim"`
-}
-
 // Init hashs var as a slice Hash struct
-var hashs []Hash
+var users []User
 
 // Get all books
-func getBooks(w http.ResponseWriter, r *http.Request) {
+func getUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(hashs)
+	json.NewEncoder(w).Encode(users)
 }
 
-// Get single book
-func getBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Gets params
-	// Loop through books and find one with the id from the params
-	for _, item := range books {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Book{})
-}
+// // Get single book
+// func getBook(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r) // Gets params
+// 	// Loop through books and find one with the id from the params
+// 	for _, item := range auths {
+// 		if item.ID == params["id"] {
+// 			json.NewEncoder(w).Encode(item)
+// 			return
+// 		}
+// 	}
+// 	json.NewEncoder(w).Encode(&Book{})
+// }
 
-// Add new book
-func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(100000000)) // Mock ID - not safe
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
-}
+// // Update book
+// func updateBook(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r)
+// 	for index, item := range books {
+// 		if item.ID == params["id"] {
+// 			books = append(books[:index], books[index+1:]...)
+// 			var book Book
+// 			_ = json.NewDecoder(r.Body).Decode(&book)
+// 			book.ID = params["id"]
+// 			books = append(books, book)
+// 			json.NewEncoder(w).Encode(book)
+// 			return
+// 		}
+// 	}
+// }
 
-// Update book
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = params["id"]
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
-			return
-		}
-	}
-}
+// // Delete book
+// func deleteBook(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r)
+// 	for index, item := range books {
+// 		if item.ID == params["id"] {
+// 			books = append(books[:index], books[index+1:]...)
+// 			break
+// 		}
+// 		json.NewEncoder(w).Encode(books)
+// 	}
+// }
 
-// Delete book
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			break
-		}
-		json.NewEncoder(w).Encode(books)
-	}
-}
+// func authenticate(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r)
+// 	for index, item := range tokens {
+// 		if item.ID == params["tokens"] {
 
-func authenticate(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range tokens {
-		if item.ID == params["tokens"] {
+// 			break
+// 		}
+// 		json.NewEncoder(w).Encode(books)
+// 	}
+// }
 
-			break
-		}
-		json.NewEncoder(w).Encode(books)
-	}
-}
-
-// StartApi - Start the api plox!
-func StartApi() {
-	// Init router
-	r := mux.NewRouter()
+// StartAPI - Start the api for testing
+func StartAPIPlease() {
 
 	// Hardcoded data - @todo: add database
-	// books = append(books, Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
-	// books = append(books, Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
+	users = append(users, User{ID: 1, Fname: "Connor", Lname: "Peters", Email: "design@mainsailstudio.com", Phone: "5854782678",
+		Security: &Security{
+			ID:   1,
+			Name: "Security level 1",
+			Desc: "This is security level 1"},
+		Locks: &Lock{
+			ID:       1,
+			UserID:   1,
+			Lock:     "1",
+			LockType: 1},
+		Auths: &Auth{
+			Auth: "testauthIguess",
+			Salt: "Iguess"},
+		KeysDisplay: &KeyDisplay{
+			ID:      1,
+			UserID:  1,
+			Key:     "test1",
+			KeyType: 1}})
 
 	// Route handles & endpoints
-	r.HandleFunc("/auth/{token}", getAuths).Methods("GET")
+	// Init router
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Accept")
+		w.Write([]byte("{\"hello\": \"world\"}"))
+	})
 
+	r.HandleFunc("/users", getUsers).Methods("GET")
+	r.HandleFunc("/user", CreateUser).Methods("POST")
+
+	// // Start server
+	// log.Fatal(http.ListenAndServe(":8000", r))
 	// Start server
-	log.Fatal(http.ListenAndServe(":8000", r))
+	handler := cors.Default().Handler(r)
+	http.ListenAndServe(":8080", handler)
+
 }
--- main.go
+++ main.go
@@ -8,27 +8,63 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/sha256"
+	dynauthapi "dynauthapi"
 	dynauthcore "dynauthcore"
 	"fmt"
-
-	"github.com/dchest/pbkdf2"
-	"golang.org/x/crypto/bcrypt"
 )
 
 // program constants
-const keyNum = 10         // this is the total amount of keys the user will want, be careful with this because the larger this number is, the factorially larger the amount of computations will be. Keep it > 30
-const displayLockNum = 4  // this is the total amount of locks that will be displayed for dynamic authentication. Same goes for this, keep it small (> 7)
-const hashIterations = 10 // currently using 10 for speed, but it is recommended to use at least 100,000 for server side
-const hashLen = 64        // 64 equals 128 characters
+const keyNum = 10        // this is the total amount of keys the user will want, be careful with this because the larger this number is, the factorially larger the amount of computations will be. Keep it > 30
+const displayLockNum = 4 // this is the total amount of locks that will be displayed for dynamic authentication. Same goes for this, keep it small (> 7)
+const hashIterations = 8 // currently using 10 for speed, but it is recommended to use at least 100,000 for server side
+const hashLen = 64       // 64 equals 128 characters
 
 func main() {
+	fmt.Println("Starting API")
+	dynauthapi.StartAPIPlease()
+	fmt.Println("1. Add a new user (auths only)")
+	fmt.Println("2. Authenticate a user")
+	var selection int
+	fmt.Scan(&selection)
+	if selection == 1 {
+		createUser()
+	} else if selection == 2 {
+		authenticateUser()
+	} else {
+		fmt.Println("Nada sir")
+	}
+} // end of main
+
+func createUser() {
 	// ask for num of lock/key combos (testing purposes)
 	fmt.Println("The default number of lock/key combos as defined by the keyNum constant are", keyNum)
 	fmt.Println("For testing purposes please enter in the number of lock/key combos you want: ")
 	var numHash int
 	fmt.Scan(&numHash)
+
+	// get user data
+	var userid string
+	var fname string
+	var lname string
+	var email string
+	var phone string
+	var securityLv string
+	fmt.Println("For testing purposes please enter in the userid (randomize please): ")
+	fmt.Scan(&userid)
+	fmt.Println("For testing purposes please enter in the firstname: ")
+	fmt.Scan(&fname)
+	fmt.Println("For testing purposes please enter in the lastname: ")
+	fmt.Scan(&lname)
+	fmt.Println("For testing purposes please enter in the email: ")
+	fmt.Scan(&email)
+	fmt.Println("For testing purposes please enter in the phone: ")
+	fmt.Scan(&phone)
+	fmt.Println("For testing purposes please enter in the security level: ")
+	fmt.Scan(&securityLv)
+
+	// store user data
+	dynauthcore.StoreUserInfo(userid, fname, lname, email, phone, securityLv)
+	fmt.Println("User info was stored")
 
 	// initialize the 2d lock-key combo slice
 	lockSlice := make([]string, numHash)
@@ -52,50 +88,34 @@ func main() {
 		fmt.Println("Key is: " + key) // print lock
 		keySlice[i] = key
 	}
+
+	// store locks
+	dynauthcore.StoreLocks(lockSlice, userid, "1") // using 1 as the locktype since it does nothing currently
+	fmt.Println("User's locks were stored")
+
+	// create and store auths
 	lockPerms := dynauthcore.LimPerms(lockSlice, displayLockNum) // create the limited permutations for the locks from the dynauthcore permutations.go package
 	keyPerms := dynauthcore.LimPerms(keySlice, displayLockNum)
 	permsToHash := dynauthcore.CombinePerms(lockPerms, keyPerms) // create the perms to hash (should most likely be in a package eventually)
 	fmt.Println("Perms to hash is", permsToHash)
 	fmt.Println("Total number of permutations is", len(permsToHash))
-	hashedPermsWithSalt := dynauthcore.HashPermsBcrypt(permsToHash, hashIterations)
+	hashedPermsWithSalt := dynauthcore.HashPermsScrypt(permsToHash, hashIterations)
 	//fmt.Println("Hashed perms is", hashedPermsWithSalt)
 	//fmt.Println("Let's try to store them!")
-	dynauthcore.StorePerms(hashedPermsWithSalt)
+	dynauthcore.StoreAuthsWithSalts(hashedPermsWithSalt, userid)
+	fmt.Println("User's auths were stored")
 	//hash(lockKeySlice)
+}
 
-} // end of main
+func authenticateUser() {
+	fmt.Println("Enter in user id")
+	var userid string
+	fmt.Scan(&userid)
+	locks := dynauthcore.ServeLocks(userid) // receives the slice of locks from serve.go
+	fmt.Println("Locks are", locks)
+	var otp string
+	fmt.Println("Enter in OTP")
+	fmt.Scan(&otp)
+	dynauthcore.AuthenticateScrypt(locks, otp, userid, hashIterations)
 
-func hash(lks [][]string) {
-
-	for i := 0; i < len(lks); i++ {
-		var OTP string
-		OTP = lks[i][1]
-		fmt.Println("OTP is", OTP)
-
-		// Hashing the password using bcrypt with the default cost of 10
-		hashedPasswordBcrypt, err := bcrypt.GenerateFromPassword([]byte(OTP), bcrypt.DefaultCost)
-		if err != nil {
-			panic(err)
-		}
-
-		// Hashing password using PBKDF2 and salt
-
-		// to make an actually random salt
-		salt := make([]byte, 32)
-		if _, err := rand.Reader.Read(salt); err != nil {
-			panic("random reader failed")
-		}
-
-		// to just use a set salt for testing
-		// stringSalt := "randomsaltforrealz"
-		// salt := []byte(stringSalt)
-		hashedPasswordPBKDF2 := pbkdf2.WithHMAC(sha256.New, []byte(OTP), salt, hashIterations, hashLen)
-
-		fmt.Println("Bcrypt hash is:", string(hashedPasswordBcrypt))
-		fmt.Println("PBKDF2 hash is:")
-		fmt.Printf("%x", hashedPasswordPBKDF2)
-		fmt.Println()
-
-	}
-
-} // end of hash
+}
